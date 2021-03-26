f9d5dc569217d0928d1ddd7579c336eb6b506489
NihadZeidan <nihadzeidan@gmail.com>
2021-03-26 20:17:32 +0300

    done

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
+    response.status(200).send('Hello');
 });
 
 app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
+    let airplanes = {
+        departure: Date.now(),
+        canFly: true,
+        pilot: 'Well Trained',
+    };
+    response.status(200).json(airplanes);
 });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
