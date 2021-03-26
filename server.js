ae067bb99518d6769045815e369b6a6496f8ded2
NihadZeidan <nihadzeidan@gmail.com>
2021-03-26 20:21:03 +0300

    doen

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
